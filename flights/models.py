46ed0bd5e22c95e03ec1b6b3d8a1820d59ed88fc
John Gutierrez <djterez88@gmail.com>
2022-07-17 20:31:40 -0700

    Created error in flights model.py to check how gihub actions work

--- flights/models.py
+++ flights/models.py
@@ -18,7 +18,7 @@ class Flight(models.Model):
         return f"{self.id}: {self.origin} to {self.destination}"
     # changing and to or for failure, for yaml
     def is_valid_flight(self):
-        return self.origin != self.destination and self.duration > 0
+        return self.origin != self.destination or self.duration > 0
 
 class Passenger(models.Model):
     first = models.CharField(max_length=64)
